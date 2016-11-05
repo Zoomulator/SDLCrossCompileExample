7e56146583dd6467cc24d6b148e79f4088a9d0d8
Kim Simmons <zoomulator@gmail.com>
2016-11-05 13:29:43 +0100

    CMake search for program in BOTH to allow finding sdl2-config.

--- Toolchain-Ubuntu-mingw32.cmake
+++ Toolchain-Ubuntu-mingw32.cmake
@@ -20,6 +20,6 @@ set(CMAKE_FIND_ROOT_PATH /usr/${TOOLCHAIN_PREFIX})
 # modify default behavior of FIND_XXX() commands to
 # search for headers/libs in the target environment and
 # search for programs in the build host environment
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
